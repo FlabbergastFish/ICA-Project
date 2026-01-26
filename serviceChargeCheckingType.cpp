82c93a12beede7ab6d72dde6e3eba3dc3eb781a9
Shanecn95 <snorris9@saddleback.edu>
2026-01-25 19:14:43 -0800

    Create serviceChargeCheckingType.cpp

---
+++ serviceChargeCheckingType.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include "serviceChargeCheckingType.h"
+
+using namespace std;
+
+const double serviceChargeCheckingType::ACCOUNT_SERVICE_CHARGE = 10.00;
+const int serviceChargeCheckingType::MAXIMUM_NUM_OF_CHECKS = 5;
+const double serviceChargeCheckingType::SERVICE_CHARGE_EXCESS_NUM_OF_CHECKS = 5;
+
+serviceChargeCheckingType::serviceChargeCheckingType(string name, int accountNumber,
+        double balance)
+        : checkingAccountType(name, accountNumber, balance)
+{
+        serviceChargeAccount = ACCOUNT_SERVICE_CHARGE;
+        numberOfChecksWritten = 0;
+        serviceChargeCheck = 0;
+}
+
+serviceChargeCheckingType::serviceChargeCheckingType(string name, int accountNumber,
+                                                                                        double balance,
+                                                                                        double servChargeAmount,
+                                                                                        double servChargeCheck)
+                                                                : checkingAccountType(name, accountNumber, balance)
+{
+        serviceChargeAccount = servChargeAmount;
+        serviceChargeCheck = servChargeCheck;
+        numberOfChecksWritten = 0;
+}
+
+double serviceChargeCheckingType::getServiceChargeAccount()
+{
+        return serviceChargeAccount;
+}
+
+void serviceChargeCheckingType::setServiceChargeAccount(double amount)
+{
+        serviceChargeAccount = amount;
+}
+
+double serviceChargeCheckingType::getServiceChargeChecks()
+{
+        return serviceChargeCheck;
+}
+
+void serviceChargeCheckingType::setServiceChargeChecks(double amount)
+{
+        serviceChargeCheck = amount;
+}
+
+int serviceChargeCheckingType::getNumberOfChecksWritten(int num)
+{
+        numberOfChecksWritten = num;
+}
+
+void serviceChargeCheckingType::postServiceCharge()
+{
+        balance = balance - serviceChargeAccount;
+}
+
+void serviceChargeCheckingType:: writeCheck(double amount)
+{
+        if (numberOfChecksWritten < MAXIMUM_NUM_OF_CHECKS)
+                balance = balance - amount;
+        else
+                balance = balance - amount - serviceChargeCheck;
+        numberOfChecksWritten++;
+}
+
+void serviceChargeCheckingType::createMonthlyStatement()
+{
+        postServiceCharge();
+}
+
+void serviceChargeCheckingType::print()
+{
+        cout << fixed << showpoint << setprecision(2);
+        cout << "Service Charge Checking: " << getName() << "\t ACCT# "
+                  << getAccountNumber() << "\tBalance: $" << getBalance();
+}
