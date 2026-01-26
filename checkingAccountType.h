a7e40ee2db0fad6f68c89eb8b66b458e37d1c461
Shanecn95 <snorris9@saddleback.edu>
2026-01-25 20:19:28 -0800

    Update checkingAccountType.h

--- checkingAccountType.h
+++ checkingAccountType.h
@@ -1,17 +1,21 @@
 #ifndef CHECKING_ACCOUNT_TYPE_H
 #define CHECKING_ACCOUNT_TYPE_H
 
+#include <string>
 #include "bankAccountType.h"
+
+using namespace std;
+
 
 class checkingAccountType: public bankAccountType
 {
-private:
-        double minimumBalance:
-        double serviceCharge:
+//private:
+//      double minimumBalance:
+//      double serviceCharge:
 
 public:
         // constructor
-        checkingAccountType(string name, int accountNumber, double balance, double minBalance, double charge);
+        checkingAccountType(string name, int accountNumber, double balance);
 
         virtual void writeCheck(double amount) = 0;
 
