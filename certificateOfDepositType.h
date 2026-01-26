da567d53035b5328a3649e4734809a5f6fb6df1c
abakhtiari1 <abakhtiari1@saddleback.edu>
2026-01-25 19:30:57 -0800

    Refactor certificateOfDepositType constructor
    Removed constructor implementation from certificateOfDepositType.

--- certificateOfDepositType.h
+++ certificateOfDepositType.h
@@ -5,12 +5,5 @@ class certificateOfDepositType : public bankAccount
 public:
     double interestRate;
     int currentCDMonth;
-    certificateOfDepositType(double interestRate, int doubleCDMonth) : bankAccount(name, accountNumber, balance)
-    {
-        this->name = name;
-        this->accountNumber = accountNumber;
-        this->balance = balance;
-        this->interestRate = interestRate;
-        this->currentCDMonth = doubleCDMonth;
-    }
+    certificateOfDepositType(double interestRate, int doubleCDMonth) : bankAccount(name, accountNumber, balance);
 };
