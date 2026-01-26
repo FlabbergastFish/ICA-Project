9d4fd47a58a4f4af8bf617fd9fc75993e467e628
Shanecn95 <snorris9@saddleback.edu>
2026-01-25 18:59:04 -0800

    Create checkingAccountType.cpp

---
+++ checkingAccountType.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include "checkingAccountType.h"
+
+using namespace std;
+
+checkingAccountType::checkingAccountType(string name, int accountNumber,
+                                                                                        double balance)
+        : bankAccountType(double minBalance, double charge);
+
+{
+}
