6ce43b66b314a8762e832892f6f26be506fce0bf
FlabbergastFish <evanoneill33@gmail.com>
2026-02-11 01:03:31 -0800

    Document file structure

--- includes/userType.h
+++ includes/userType.h
@@ -17,6 +17,53 @@
 #include "certificateOfDepositType.h"
 #include "checkingAccountType.h"
 
+/***************************************************************************************
+ * Data/File Structure Documentation
+ * *************************************************************************************
+ * Generic User Directory Layout
+ * -------------------------------------------------------------------------------------
+ * Folders are followed by a '/', and anything surrounded with <> is a dynamic names.
+ * -------------------------------------------------------------------------------------
+ * 
+ * data/
+ * 	|-- accounts/
+ * 	|	 |-- <username>/
+ * 	|	 |	   |-- data.txt
+ * 	|	 |	   |-- passwd.txt
+ * 	|	 |	   |-- pending_transfers/
+ * 	|	 |	   |	| -- <account#>.txt
+ * 	|	 |	   |	| -- <secondAccount#>.txt
+ * 	|	 |	   |	L -- <...Account#>.txt
+ * 	|	 |	   |-- <account#>.txt
+ * 	|	 |	   |-- <secondAccount#>.txt
+ * 	|	 |	   L-- <...Account#>.txt
+ * 	|	 |-- <username>/
+ * 	|	 |	   |-- ...
+ * 
+ * *************************************************************************************
+ * File Layouts
+ * -------------------------------------------------------------------------------------
+ * 
+ * data/accounts/<username>/data.txt
+ * 	 Line 1:	name		 	(string)				e.g. Jane Doe
+ * 	 Line 2:	manager     	(bool)					e.g. 0
+ * 	 Line 3:	salt        	(string)				e.g. h23k8JO1K
+ * 
+ * data/accounts/<username>/passwd.txt
+ * 	 Line 1:	password 	    (string)				e.g. badPass!
+ * 
+ * data/accounts/<username>/<acount#>.txt
+ * 	 Line 1:	accountType 	(int/enum bankingType)	e.g. 0
+ * 	 Line 2:	frozen		 	(bool)					e.g. 0
+ * 	 Line 3:	transaction 	(signed double)			e.g. -23.12
+ * 	 Line 4+:	transaction 	(signed double)			e.g. 71.84
+ * 
+ * data/accounts/<username>/pending_transfers/<acount#>.txt
+ * 	 Line 1:	transaction 	(double)				e.g. 12.96
+ * 	 Line 2+:	transaction 	(double)				e.g. 38.39
+ * 
+ ***************************************************************************************/
+
 const string USER_DIR = "data/accounts";
 const string DATA_FILE = "data.txt";
 const string PASSWD_FILE = "passwd.txt";
